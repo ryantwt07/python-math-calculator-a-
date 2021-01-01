1ff6ab1a900a4c853c57464c74729691d2607f7a
CodeAsherPlayz <ryanthum202@gmail.com>
2021-01-01 09:55:00 +0800

    Making Code More Readable
    This was a pull request made by a user. Thanks!

--- version1-0.py
+++ version1-0.py
@@ -1,86 +1,34 @@
-# Variables
+try:
+    # Take input command
+    cmd = int(input("""
+Enter 1 for area of Square
+Enter 2 for perimeter of Square
+Enter 3 for area of Rectangle
+Enter 4 for perimeter of Rectangle
+>>> """))
+except ValueError:
+    print("Check input and try again")
+else:
+    # Just giving a newline
+    print()
+    # For square
+    if cmd in [1, 2]:
+        l = float(input("Enter length of sides of Square: "))
 
-q1 = True
-q2 = False
-q3 = False
-calculate = False
-ask = False
-q1a = ""
-q2a = ""
-square_length = ""
-square_area = ""
-square_perimeter = ""
-rectangle_length = ""
-rectangle_breath = ""
-rectangle_area = ""
-rectangle_perimeter = ""
-rectangle_perimeter_1 = ""
-rectangle_perimeter_2 = ""
-
-# Systems
-
-if q1:
-  q1a = input("What shape would you like to calculate? Enter Here: ")
-  q1 = False
-  q2 = True
-  
-if q2:
-  if q1a == "Square":
-    q2a = input("Area or Perimeter? Enter Here: ")
-    q2 = False
-    q3 = True
-  elif q1a == "Rectangle":
-    q2a = input("Area or Perimeter? Enter Here: ")
-    q2 = False
-    q3 = True
-    
-if q3:
-  if q1a == "Square":
-    if q2a == "Area":
-      square_length = input("What is the length of the square? Enter Here: ")
-      q3 = False
-      calculate = True
-    elif q2a == "Perimeter":
-      square_length = input("What is the length of the square? Enter Here: ")
-      q3 = False
-      calculate = True
-  elif q1a == "Rectangle":
-    if q2a == "Area":
-      rectangle_length = input("What is the length of the rectangle? Enter Here: ")
-      rectangle_breath = input("What is the breath of the rectangle? Enter Here: ")
-      q3 = False
-      calculate = True
-    elif q2a == "Perimeter":
-      rectangle_length = input("What is the length of the rectangle? Enter Here: ")
-      rectangle_breath = input("What is the breath of the rectangle? Enter Here: ")
-      q3 = False
-      calculate = True
-
-if calculate:
- if q1a == "Square":
-  if q2a == "Area":
-   square_area = square_length * square_length
-  elif q2a == "Perimeter":
-   square_perimeter = square_length * 4
- elif q1a == "Rectangle"
-  if q2a == "Area":
-   rectangle_area = rectangle_length * rectangle_breath
-   calculate = False
-  if q2a == "Perimeter":
-   rectangle_perimeter_1 = rectangle_length * 2
-   rectangle_perimeter_2 = rectangle_breath * 2
-   rectangle_perimeter = rectangle_perimeter_1 + rectangle_perimeter_2
-   calculate = False
-
-if ask:
- if q1a == "Square":
-  if q2a == "Area": 
-   print("The area of the square is " + square_area + " !")
-  elif q2a == "Perimeter":
-   print("The perimeter of the square is " + square_perimeter + " !")
- elif q1a == "Rectangle":
-  if q2a == "Area":
-    print("The area of the rectangle is " + rectangle_area + " !")
-  elif q2a == "Perimeter":
-    print("The perimeter of the rectangle is " + rectangle_perimeter + " !")
- 
+        # For area of square.
+        if cmd == 1:
+            print(l * l)
+        # For perimeter of square.
+        else:
+            print(4 * l)
+    elif cmd in [3, 4]:
+        l = float(input("Enter length of Rectangle: "))
+        b = float(input("Enter width of Rectangle: "))
+        # For area of Rectangle
+        if cmd == 3:
+            print(l * b)
+        #For perimeter of Ractangle
+        else:
+            print(2 * (l + b))
+    else:
+        print("Check input and try again.")
