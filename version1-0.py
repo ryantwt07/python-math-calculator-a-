e2c8677600c1860edf9dccc643dbef72ed36f9cc
CodeAsherPlayz <ryanthum202@gmail.com>
2021-01-01 10:08:39 +0800

    Publishing Update

--- version1-0.py
+++ version1-0.py
@@ -1,34 +1,34 @@
 try:
-    # Take input command
+    # Take Input Command
     cmd = int(input("""
-Enter 1 for area of Square
-Enter 2 for perimeter of Square
-Enter 3 for area of Rectangle
-Enter 4 for perimeter of Rectangle
+Enter 1 for Area of Square
+Enter 2 for Perimeter of Square
+Enter 3 for Area of Rectangle
+Enter 4 for Perimeter of Rectangle
 >>> """))
 except ValueError:
-    print("Check input and try again")
+    print("You have entered an invalid input. Please try again.")
 else:
     # Just giving a newline
     print()
     # For square
     if cmd in [1, 2]:
-        l = float(input("Enter length of sides of Square: "))
+        length = float(input("Enter length of sides of Square: "))
 
         # For area of square.
         if cmd == 1:
-            print(l * l)
+            print(length * length)
         # For perimeter of square.
         else:
-            print(4 * l)
+            print(4 * length)
     elif cmd in [3, 4]:
-        l = float(input("Enter length of Rectangle: "))
-        b = float(input("Enter width of Rectangle: "))
+        length = float(input("Enter length of Rectangle: "))
+        breath = float(input("Enter width of Rectangle: "))
         # For area of Rectangle
         if cmd == 3:
-            print(l * b)
+            print(length * breath)
         #For perimeter of Ractangle
         else:
-            print(2 * (l + b))
+            print(2 * (length + breath))
     else:
-        print("Check input and try again.")
+        print("You have entered in an invalid input. Please try again.")
