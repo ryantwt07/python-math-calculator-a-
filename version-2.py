0c223264b240ff32f72263b87fe009c7a98d605e
CodeAsherPlayz <ryanthum202@gmail.com>
2021-01-02 13:20:24 +0800

    More Functions

--- version-2.py
+++ version-2.py
@@ -6,7 +6,11 @@ Enter 2 for Perimeter of Square
 Enter 3 for Area of Rectangle
 Enter 4 for Perimeter of Rectangle
 Enter 5 for Area of Circle (pi will be 3.14)
-Enter 6 for Diameter of Circle
+Enter 6 for Diameter of Circle (pi will be 3.14)
+Enter 7 for Circumference of Circle (pi will be 3.14)
+Enter 8 for Area of Triangle
+Enter 9 for Perimeter of Triangle
+Enter 10 for Area of Trapezium
 >>> """))
 except ValueError:
     print("You have entered an invalid input. Please try again.")
@@ -32,11 +36,31 @@ else:
         # For perimeter of Rectangle
         else:
             print(2 * (length + breath))
-    elif cmd in [5, 6]:
+    elif cmd in [5, 6, 7]:
         radius = float(input("Enter radius of circle: "))
         if cmd == 5:
             print(3.14 * (radius * radius))
         elif cmd == 6:
             print(3.14 * radius)
+        elif cmd == 7:
+            print(3.14 * (radius * 2))
+    elif cmd == 8:
+        base = float(input("Enter base of triangle: "))
+        height = float(input("Enter height of triangle: "))
+        print(0.5 * base * height)
+    elif cmd == 9:
+        base = float(input("Enter base of triangle: "))
+        base2 = float(input("Enter base 2 of triangle: "))
+        base3 = float(input("Enter base 3 of triangle: "))
+        print(base1 + base2 + base3)
+    elif cmd in [10,11]:
+        a = float(input("Enter base of trapezium: "))
+        b = float(input("Enter top length of trapezium: "))
+        height = float(input("Enter height of trapezim: "))]
+        if cmd == 10:
+            print(0.5 * (a + b) * height)
+        elif cmd == 11:
+            width = float(input("Enter width of trapezium: "))
+            print((0.5 * (a + b) * height) * width)
     else:
         print("You have entered in an invalid input. Please try again.")
